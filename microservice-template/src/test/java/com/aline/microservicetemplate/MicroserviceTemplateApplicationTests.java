9ae486da0bef2c17e822a49c5db8310147cf1fb2
Leandro Yabut <80281802+leandroyabut@users.noreply.github.com>
2021-09-23 09:38:09 -0600

    Initial commit

---
+++ microservice-template/src/test/java/com/aline/microservicetemplate/MicroserviceTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aline.microservicetemplate;
+
+import com.aline.core.annotation.test.SpringBootIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootIntegrationTest
+class MicroserviceTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
