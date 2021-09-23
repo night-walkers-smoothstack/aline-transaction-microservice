534fb72832f249fe3e0bb7b20b2c02a69319943a
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-23 11:02:29 -0600

    AF-267 Add health check endpoint and coverage.

--- transaction-microservice/src/test/java/com/aline/transactionmicroservice/TransactionMicroserviceApplicationTests.java
+++ transaction-microservice/src/test/java/com/aline/transactionmicroservice/TransactionMicroserviceApplicationTests.java
@@ -2,12 +2,19 @@ package com.aline.transactionmicroservice;
 
 import com.aline.core.annotation.test.SpringBootIntegrationTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootIntegrationTest
 class TransactionMicroserviceApplicationTests {
 
+    @Autowired
+    TransactionMicroserviceApplication application;
+
     @Test
     void contextLoads() {
+        assertNotNull(application);
     }
 
 }
