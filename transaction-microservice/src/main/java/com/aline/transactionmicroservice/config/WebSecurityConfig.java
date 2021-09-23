740b6959e750665c43da0a25c9f87297c4d62fa8
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-23 17:37:36 -0600

    AF-267 Add web security config

---
+++ transaction-microservice/src/main/java/com/aline/transactionmicroservice/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.aline.transactionmicroservice.config;
+
+import com.aline.core.annotation.WebSecurityConfiguration;
+import com.aline.core.security.config.AbstractWebSecurityConfig;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@WebSecurityConfiguration
+public class WebSecurityConfig extends AbstractWebSecurityConfig {
+    @Override
+    protected void configureHttp(HttpSecurity http) {
+
+    }
+}
