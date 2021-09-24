b36e32575dcabfb0bd0af31b3099c7f21d648146
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-24 01:16:35 -0600

    AF-267 Add more transactions

--- transaction-microservice/src/test/resources/scripts/transactions.sql
+++ transaction-microservice/src/test/resources/scripts/transactions.sql
@@ -8,17 +8,30 @@ values (2, '123 Little Blvd', 'City', '2021-08-06', '1997-06-02', 'DC654987', 't
         'Test', 'MALE', 30000000,  '2021-08-21', 'Man', '123 Little Blvd', 'City', 'Maine', '12345',
         'The', '(555) 888-8888', '888-88-8888', 'Maine', '12345');
 
+insert into applicant (id, address, city, created_at, date_of_birth, drivers_license, email, first_name, gender, income,
+                       last_modified_at, last_name, mailing_address, mailing_city, mailing_state, mailing_zipcode,
+                       middle_name, phone, social_security, state, zipcode)
+values (3, '123 Little Blvd', 'City', '2021-08-06', '1997-07-02', 'DC845454', 'test_woman@email.com',
+        'Test', 'FEMALE', 30000000,  '2021-08-21', 'Woman', '123 Little Blvd', 'City', 'Maine', '12345',
+        'The', '(555) 888-8888', '888-88-8889', 'Maine', '12345');
+
 insert into member (id, membership_id, applicant_id, branch_id)
 values (1, '12345678', 1, 1);
 
 insert into member (id, membership_id, applicant_id, branch_id)
 values (2, '87654321', 2, 1);
 
+insert into member (id, membership_id, applicant_id, branch_id)
+values (3, '98765432', 3, 1);
+
 insert into user (role, id, enabled, password, username, member_id)
 values ('MEMBER', 1, 1, 'P@ssword123', 'test_boy', 1);
 
 insert into user (role, id, enabled, password, username, member_id)
-values ('ADMINISTRATOR', 2, 1, 'P@ssword123', 'test_man', 2);
+values ('MEMBER', 2, 1, 'P@ssword123', 'test_man', 2);
+
+insert into user (role, id, enabled, password, username, member_id)
+values ('MEMBER', 3, 1, 'P@ssword123', 'test_woman', 3);
 
 insert into account (account_type, id, account_number, balance, status, available_balance,
                      primary_account_holder_id)
@@ -35,10 +48,23 @@ values ('CHECKING', 3, '0011011235', 100000000, 'ACTIVE', 100000000, 2);
 insert into account_holder (member_id, account_id)
 values (1, 1),
        (1, 2),
-       (2, 3);
+       (2, 3),
+       (3, 3);
 
 insert into transaction (id, type, method, amount, date, account_id, initial_balance, posted_balance, status)
 values (1, 'WITHDRAWAL', 'ACH', 10000, '2021-08-01', 1, 100000, 90000, 'APPROVED');
 
 insert into transaction (id, type, method, amount, date, account_id, initial_balance, posted_balance, status)
 values (2, 'PURCHASE', 'CARD', 10000, '2021-08-01', 1, 90000, 80000, 'APPROVED');
+
+insert into transaction (id, type, method, amount, date, account_id, initial_balance, posted_balance, status)
+values (3, 'PURCHASE', 'CARD', 10000, '2021-08-01', 3, 50000, 80000, 'APPROVED');
+
+insert into transaction (id, type, method, amount, date, account_id, initial_balance, posted_balance, status)
+values (4, 'DEPOSIT', 'ACH', 500000, '2021-08-01', 3, 1300000, 80000, 'APPROVED');
+
+insert into transaction (id, type, method, amount, date, account_id, initial_balance, posted_balance, status)
+values (5, 'DEPOSIT', 'ACH', 500000, '2021-08-01', 1, 1300000, 80000, 'APPROVED');
+
+insert into transaction (id, type, method, amount, date, account_id, initial_balance, posted_balance, status)
+values (6, 'WITHDRAWAL', 'ACH', 500000, '2021-08-01', 3, 1800000, 1300000, 'APPROVED');
