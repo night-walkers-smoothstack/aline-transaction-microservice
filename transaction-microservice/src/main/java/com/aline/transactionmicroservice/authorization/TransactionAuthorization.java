e2e8939b27f3321215aa474598672d01d083dc9a
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-23 19:16:18 -0600

    AF-267 Remove unused variables from can access method

--- transaction-microservice/src/main/java/com/aline/transactionmicroservice/authorization/TransactionAuthorization.java
+++ transaction-microservice/src/main/java/com/aline/transactionmicroservice/authorization/TransactionAuthorization.java
@@ -52,7 +52,6 @@ public class TransactionAuthorization extends AbstractAuthorizationService<Trans
     public boolean canAccessById(long id) {
         if (getRole() == UserRole.MEMBER) {
             MemberUser user = (MemberUser) getUser();
-            Member member = user.getMember();
             Account account = accountService.getAccountById(id);
             Transaction transaction = repository.findById(id).orElseThrow(TransactionNotFoundException::new);
             return transaction.getAccount().equals(account);
