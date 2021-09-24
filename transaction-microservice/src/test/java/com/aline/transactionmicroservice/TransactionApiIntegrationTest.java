d733262748e4d1e1b83599cd1c84e29d6b03e433
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-23 19:13:17 -0600

    AF-267 Optimize imports

--- transaction-microservice/src/test/java/com/aline/transactionmicroservice/TransactionApiIntegrationTest.java
+++ transaction-microservice/src/test/java/com/aline/transactionmicroservice/TransactionApiIntegrationTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import javax.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
